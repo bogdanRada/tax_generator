425709da62fcd8a419cbfeaa4d3b2e2a7c20f92b
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-17 08:56:39 +0200

    delete initializers

--- lib/tax_generator/initializers/celluloid.rb
+++
@@ -1,9 +0,0 @@
-Celluloid::Logger::WithBacktrace.class_eval do
-  alias_method :original_decorate, :decorate
-
-  def decorate(string)
-    return if string.include?('signaled spuriously')
-    original_decorate(string)
-  end
-
-end

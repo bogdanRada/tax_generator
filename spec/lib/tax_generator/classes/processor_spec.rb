8f507560926332e81bfe02042c8bbda6ba07028b
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-17 19:25:33 +0200

    more tests

--- spec/lib/tax_generator/classes/processor_spec.rb
+++ spec/lib/tax_generator/classes/processor_spec.rb
@@ -37,11 +37,46 @@ describe TaxGenerator::Processor do
   end
 
   context "intialize" do
-    let(:subject) {TaxGenerator::Processor.new}
 
     specify { expect(subject.jobs).to eq({}) }
     specify { expect(subject.job_to_worker).to eq({}) }
     specify { expect(subject.worker_to_job).to eq({}) }
+  end
+
+
+
+  it 'returns the input folder' do
+    subject.options.expects(:fetch).with(:input_dir, "#{root}/data/input")
+    subject.input_folder
+  end
+
+  it 'returns the output_folder' do
+    subject.options.expects(:fetch).with(:output_dir, "#{root}/data/output")
+    subject.output_folder
+  end
+
+  it 'returns the taxonomy_filename' do
+    subject.options.expects(:fetch).with(:taxonomy_filename, 'taxonomy.xml')
+    subject.taxonomy_file_name
+  end
+
+  it 'returns the destinations_file_name' do
+    subject.options.expects(:fetch).with(:destinations_filename, 'destinations.xml')
+    subject.destinations_file_name
+  end
+
+  it 'returns the taxonomy_file_path' do
+    File.expects(:join).with(subject.input_folder, subject.taxonomy_file_name).returns(true)
+    subject.taxonomy_file_path
+  end
+  it 'returns the destinations_file_path' do
+    File.expects(:join).with(subject.input_folder, subject.destinations_file_name).returns(true)
+    subject.destinations_file_path
+  end
+
+  it 'returns the static_output_dir' do
+    File.expects(:join).with(subject.output_folder, 'static').returns(true)
+    subject.static_output_dir
   end
 
 

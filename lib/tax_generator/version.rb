5a075a52a38b78c94d6589d29ae1f5b9fdd539de
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-18 10:30:45 +0200

    update template

--- lib/tax_generator/version.rb
+++ lib/tax_generator/version.rb
@@ -15,7 +15,7 @@ module TaxGenerator
     # major release version
     MAJOR = 0
     # minor release version
-    MINOR = 4
+    MINOR = 5
     # tiny release version
     TINY = 0
     # prelease version ( set this only if it is a prelease)
